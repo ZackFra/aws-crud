8ecb73bcff04eef7ee346882375028320aab7851
ZackFra <zack11frazier22@gmail.com>
2024-11-04 21:15:57 -0800

    Fix typo in permissions comment in TodoStack

--- lib/todo-stack.ts
+++ lib/todo-stack.ts
@@ -72,7 +72,7 @@ export class TodoStack extends cdk.Stack {
       }
     })
 
-    /** permissiones  */
+    /** permissions  */
     taskTable.grantReadWriteData(removeAttachmentFn)
 
     attachmentsBucket.grantPut(uploadAttachmentFn)
